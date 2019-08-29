07a06485349fe2c3505d160ee49a8ecd885311d9
petergphillips <peter@greenthistle.co.uk>
2019-08-29 09:04:52 +0100

    DT-180: Add in dependency on case notes service (#70)
    * DT-180: Add in dependency on case notes service
    
    * DT-180: Fix tpyo / cut and paste error

---
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/health/CaseNotesApiHealth.java
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.whereabouts.services.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CaseNotesApiHealth extends HealthCheck {
+
+    @Autowired
+    public CaseNotesApiHealth(@Qualifier("caseNotesApiRestTemplate") final RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+}
