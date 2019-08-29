da104a5d4ad5df56020d05077ccf61e229affc8e
Peter Phillips <peter@greenthistle.co.uk>
2019-08-29 07:01:50 +0100

    DT-180: Upgrade libs and improve health check

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/health/HealthCheck.java
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/health/HealthCheck.java
@@ -1,39 +1,26 @@
 package uk.gov.justice.digital.hmpps.whereabouts.services.health;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import static lombok.AccessLevel.PROTECTED;
 
+
+@AllArgsConstructor(access = PROTECTED)
 public abstract class HealthCheck implements HealthIndicator {
     private final RestTemplate restTemplate;
-
-    protected HealthCheck(final RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     @Override
     public Health health() {
         try {
             final var responseEntity = this.restTemplate.getForEntity("/ping", String.class);
-            return health(responseEntity.getStatusCode());
+            return Health.up().withDetail("HttpStatus", responseEntity.getStatusCode()).build();
         } catch (final RestClientException e) {
-            return health(HttpStatus.SERVICE_UNAVAILABLE);
+            return Health.down(e).build();
         }
-    }
-
-    private Health health(final HttpStatus code) {
-        return health (
-                Health.up(),
-                code);
-    }
-
-    private Health health(final Health.Builder builder, final HttpStatus code) {
-        return builder
-                .withDetail("HttpStatus", code.value())
-                .build();
     }
 }
