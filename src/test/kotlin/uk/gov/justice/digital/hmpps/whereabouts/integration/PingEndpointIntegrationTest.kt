93fcf3d08b6920d9f3a6f276c8e83e030398e7f1
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2019-10-22 11:24:07 +0100

    DT-320: added telemetry module to log in app insights the client ID and username of each API request (#86)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/integration/PingEndpointIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/integration/PingEndpointIntegrationTest.kt
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.util.MimeType
 
 @RunWith(SpringRunner::class)
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PingEndpointIntegrationTest {
   @Autowired
