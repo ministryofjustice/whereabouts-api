75547f373e689eaa14747961662f172f01ac2ff5
JamieDraperUK <draper65@googlemail.com>
2019-11-11 10:09:25 +0000

    NN-2151 improve absences response

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/AbsenceResponse.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/AbsenceResponse.kt
@@ -2,13 +2,13 @@ package uk.gov.justice.digital.hmpps.whereabouts.dto
 
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
+import lombok.AllArgsConstructor
+import lombok.Builder
+import lombok.Data
+import lombok.NoArgsConstructor
 
-
-@ApiModel(description = "Attendances response")
-data class AttendancesResponse(
-    @ApiModelProperty(value = "Set of attendances")
-    var attendances: Set<AttendanceDto>? = null,
-
+@ApiModel(description = "Absences response")
+open data class AbsencesResponse(
     @ApiModelProperty(value = "Set of absences")
     var absences: Set<AbsenceDto>? = null
 )
