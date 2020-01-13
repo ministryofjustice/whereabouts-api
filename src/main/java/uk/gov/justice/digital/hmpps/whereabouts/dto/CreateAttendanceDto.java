dbfd20a79f119d3309a821c468d741598be49eb3
JamieDraperUK <draper65@googlemail.com>
2020-01-13 10:26:45 +0000

    NN-2363 convert attendance service into kotlin

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/CreateAttendanceDto.java
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/CreateAttendanceDto.java
@@ -3,10 +3,6 @@ package uk.gov.justice.digital.hmpps.whereabouts.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import uk.gov.justice.digital.hmpps.whereabouts.model.AbsentReason;
 import uk.gov.justice.digital.hmpps.whereabouts.model.TimePeriod;
@@ -14,10 +10,6 @@ import uk.gov.justice.digital.hmpps.whereabouts.model.TimePeriod;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
 @ApiModel(description = "Attendance details to create")
 public class CreateAttendanceDto {
     @ApiModelProperty(required = true, value = "Id of active booking", example = "1")
@@ -59,4 +51,256 @@ public class CreateAttendanceDto {
     @Length(max = 240)
     @ApiModelProperty(value = "Comments about non attendance. This also gets used for the IEP warning text ")
     private String comments;
+
+    public CreateAttendanceDto(@NotNull Long bookingId, @NotNull Long eventId, @NotNull Long eventLocationId, @NotNull TimePeriod period, @NotNull String prisonId, @NotNull Boolean attended, @NotNull Boolean paid, AbsentReason absentReason, @NotNull LocalDate eventDate, @Length(max = 240) String comments) {
+        this.bookingId = bookingId;
+        this.eventId = eventId;
+        this.eventLocationId = eventLocationId;
+        this.period = period;
+        this.prisonId = prisonId;
+        this.attended = attended;
+        this.paid = paid;
+        this.absentReason = absentReason;
+        this.eventDate = eventDate;
+        this.comments = comments;
+    }
+
+    public CreateAttendanceDto() {
+    }
+
+    public static CreateAttendanceDtoBuilder builder() {
+        return new CreateAttendanceDtoBuilder();
+    }
+
+    public @NotNull Long getBookingId() {
+        return this.bookingId;
+    }
+
+    public @NotNull Long getEventId() {
+        return this.eventId;
+    }
+
+    public @NotNull Long getEventLocationId() {
+        return this.eventLocationId;
+    }
+
+    public @NotNull TimePeriod getPeriod() {
+        return this.period;
+    }
+
+    public @NotNull String getPrisonId() {
+        return this.prisonId;
+    }
+
+    public @NotNull Boolean getAttended() {
+        return this.attended;
+    }
+
+    public @NotNull Boolean getPaid() {
+        return this.paid;
+    }
+
+    public AbsentReason getAbsentReason() {
+        return this.absentReason;
+    }
+
+    public @NotNull LocalDate getEventDate() {
+        return this.eventDate;
+    }
+
+    public @Length(max = 240) String getComments() {
+        return this.comments;
+    }
+
+    public void setBookingId(@NotNull Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public void setEventId(@NotNull Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setEventLocationId(@NotNull Long eventLocationId) {
+        this.eventLocationId = eventLocationId;
+    }
+
+    public void setPeriod(@NotNull TimePeriod period) {
+        this.period = period;
+    }
+
+    public void setPrisonId(@NotNull String prisonId) {
+        this.prisonId = prisonId;
+    }
+
+    public void setAttended(@NotNull Boolean attended) {
+        this.attended = attended;
+    }
+
+    public void setPaid(@NotNull Boolean paid) {
+        this.paid = paid;
+    }
+
+    public void setAbsentReason(AbsentReason absentReason) {
+        this.absentReason = absentReason;
+    }
+
+    public void setEventDate(@NotNull LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setComments(@Length(max = 240) String comments) {
+        this.comments = comments;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof CreateAttendanceDto)) return false;
+        final CreateAttendanceDto other = (CreateAttendanceDto) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$bookingId = this.getBookingId();
+        final Object other$bookingId = other.getBookingId();
+        if (this$bookingId == null ? other$bookingId != null : !this$bookingId.equals(other$bookingId)) return false;
+        final Object this$eventId = this.getEventId();
+        final Object other$eventId = other.getEventId();
+        if (this$eventId == null ? other$eventId != null : !this$eventId.equals(other$eventId)) return false;
+        final Object this$eventLocationId = this.getEventLocationId();
+        final Object other$eventLocationId = other.getEventLocationId();
+        if (this$eventLocationId == null ? other$eventLocationId != null : !this$eventLocationId.equals(other$eventLocationId))
+            return false;
+        final Object this$period = this.getPeriod();
+        final Object other$period = other.getPeriod();
+        if (this$period == null ? other$period != null : !this$period.equals(other$period)) return false;
+        final Object this$prisonId = this.getPrisonId();
+        final Object other$prisonId = other.getPrisonId();
+        if (this$prisonId == null ? other$prisonId != null : !this$prisonId.equals(other$prisonId)) return false;
+        final Object this$attended = this.getAttended();
+        final Object other$attended = other.getAttended();
+        if (this$attended == null ? other$attended != null : !this$attended.equals(other$attended)) return false;
+        final Object this$paid = this.getPaid();
+        final Object other$paid = other.getPaid();
+        if (this$paid == null ? other$paid != null : !this$paid.equals(other$paid)) return false;
+        final Object this$absentReason = this.getAbsentReason();
+        final Object other$absentReason = other.getAbsentReason();
+        if (this$absentReason == null ? other$absentReason != null : !this$absentReason.equals(other$absentReason))
+            return false;
+        final Object this$eventDate = this.getEventDate();
+        final Object other$eventDate = other.getEventDate();
+        if (this$eventDate == null ? other$eventDate != null : !this$eventDate.equals(other$eventDate)) return false;
+        final Object this$comments = this.getComments();
+        final Object other$comments = other.getComments();
+        if (this$comments == null ? other$comments != null : !this$comments.equals(other$comments)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof CreateAttendanceDto;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $bookingId = this.getBookingId();
+        result = result * PRIME + ($bookingId == null ? 43 : $bookingId.hashCode());
+        final Object $eventId = this.getEventId();
+        result = result * PRIME + ($eventId == null ? 43 : $eventId.hashCode());
+        final Object $eventLocationId = this.getEventLocationId();
+        result = result * PRIME + ($eventLocationId == null ? 43 : $eventLocationId.hashCode());
+        final Object $period = this.getPeriod();
+        result = result * PRIME + ($period == null ? 43 : $period.hashCode());
+        final Object $prisonId = this.getPrisonId();
+        result = result * PRIME + ($prisonId == null ? 43 : $prisonId.hashCode());
+        final Object $attended = this.getAttended();
+        result = result * PRIME + ($attended == null ? 43 : $attended.hashCode());
+        final Object $paid = this.getPaid();
+        result = result * PRIME + ($paid == null ? 43 : $paid.hashCode());
+        final Object $absentReason = this.getAbsentReason();
+        result = result * PRIME + ($absentReason == null ? 43 : $absentReason.hashCode());
+        final Object $eventDate = this.getEventDate();
+        result = result * PRIME + ($eventDate == null ? 43 : $eventDate.hashCode());
+        final Object $comments = this.getComments();
+        result = result * PRIME + ($comments == null ? 43 : $comments.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "CreateAttendanceDto(bookingId=" + this.getBookingId() + ", eventId=" + this.getEventId() + ", eventLocationId=" + this.getEventLocationId() + ", period=" + this.getPeriod() + ", prisonId=" + this.getPrisonId() + ", attended=" + this.getAttended() + ", paid=" + this.getPaid() + ", absentReason=" + this.getAbsentReason() + ", eventDate=" + this.getEventDate() + ", comments=" + this.getComments() + ")";
+    }
+
+    public CreateAttendanceDtoBuilder toBuilder() {
+        return new CreateAttendanceDtoBuilder().bookingId(this.bookingId).eventId(this.eventId).eventLocationId(this.eventLocationId).period(this.period).prisonId(this.prisonId).attended(this.attended).paid(this.paid).absentReason(this.absentReason).eventDate(this.eventDate).comments(this.comments);
+    }
+
+    public static class CreateAttendanceDtoBuilder {
+        private @NotNull Long bookingId;
+        private @NotNull Long eventId;
+        private @NotNull Long eventLocationId;
+        private @NotNull TimePeriod period;
+        private @NotNull String prisonId;
+        private @NotNull Boolean attended;
+        private @NotNull Boolean paid;
+        private AbsentReason absentReason;
+        private @NotNull LocalDate eventDate;
+        private @Length(max = 240) String comments;
+
+        CreateAttendanceDtoBuilder() {
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder bookingId(@NotNull Long bookingId) {
+            this.bookingId = bookingId;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder eventId(@NotNull Long eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder eventLocationId(@NotNull Long eventLocationId) {
+            this.eventLocationId = eventLocationId;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder period(@NotNull TimePeriod period) {
+            this.period = period;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder prisonId(@NotNull String prisonId) {
+            this.prisonId = prisonId;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder attended(@NotNull Boolean attended) {
+            this.attended = attended;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder paid(@NotNull Boolean paid) {
+            this.paid = paid;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder absentReason(AbsentReason absentReason) {
+            this.absentReason = absentReason;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder eventDate(@NotNull LocalDate eventDate) {
+            this.eventDate = eventDate;
+            return this;
+        }
+
+        public CreateAttendanceDto.CreateAttendanceDtoBuilder comments(@Length(max = 240) String comments) {
+            this.comments = comments;
+            return this;
+        }
+
+        public CreateAttendanceDto build() {
+            return new CreateAttendanceDto(bookingId, eventId, eventLocationId, period, prisonId, attended, paid, absentReason, eventDate, comments);
+        }
+
+        public String toString() {
+            return "CreateAttendanceDto.CreateAttendanceDtoBuilder(bookingId=" + this.bookingId + ", eventId=" + this.eventId + ", eventLocationId=" + this.eventLocationId + ", period=" + this.period + ", prisonId=" + this.prisonId + ", attended=" + this.attended + ", paid=" + this.paid + ", absentReason=" + this.absentReason + ", eventDate=" + this.eventDate + ", comments=" + this.comments + ")";
+        }
+    }
 }
---
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/AttendanceService.kt
@@ -0,0 +1,306 @@
+package uk.gov.justice.digital.hmpps.whereabouts.services
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.whereabouts.dto.*
+import uk.gov.justice.digital.hmpps.whereabouts.model.AbsentReason
+import uk.gov.justice.digital.hmpps.whereabouts.model.Attendance
+import uk.gov.justice.digital.hmpps.whereabouts.model.TimePeriod
+import uk.gov.justice.digital.hmpps.whereabouts.repository.AttendanceRepository
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.function.Predicate
+import java.util.stream.Collectors
+import javax.transaction.Transactional
+
+@Service
+open class AttendanceService(private val attendanceRepository: AttendanceRepository, private val elite2ApiService: Elite2ApiService, private val iepWarningService: IEPWarningService) {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+    val nomisEventOutcomeMapper: NomisEventOutcomeMapper = NomisEventOutcomeMapper()
+  }
+
+  val absenceReasons: AbsentReasonsDto
+    get() = AbsentReasonsDto(
+        AbsentReason.getPaidReasons(),
+        AbsentReason.getUnpaidReasons(),
+        AbsentReason.getIepTriggers()
+    )
+
+  open fun getAttendanceForEventLocation(prisonId: String?, eventLocationId: Long?, date: LocalDate?, period: TimePeriod?): Set<AttendanceDto> {
+    val attendance = attendanceRepository
+        .findByPrisonIdAndEventLocationIdAndEventDateAndPeriod(prisonId, eventLocationId, date, period)
+
+    return attendance
+        .stream()
+        .map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }
+        .collect(Collectors.toSet())
+  }
+
+  open fun getAbsencesForReason(prisonId: String?, date: LocalDate?, period: TimePeriod?): Set<AttendanceDto> {
+    val attendance = attendanceRepository
+        .findByPrisonIdAndEventDateAndPeriodAndAbsentReasonNotNull(prisonId, date, period)
+
+    return attendance
+        .stream()
+        .map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }
+        .collect(Collectors.toSet())
+  }
+
+  open fun getAttendanceForBookings(prisonId: String?, bookings: Set<Long?>?, date: LocalDate?, period: TimePeriod?): Set<AttendanceDto> {
+    val attendance = attendanceRepository
+        .findByPrisonIdAndBookingIdInAndEventDateAndPeriod(prisonId, bookings, date, period)
+
+    return attendance
+        .stream()
+        .map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }
+        .collect(Collectors.toSet())
+  }
+
+  @Transactional
+  @Throws(AttendanceExists::class)
+  open fun createAttendance(attendanceDto: CreateAttendanceDto): AttendanceDto {
+    val provisionalAttendance = toAttendance(attendanceDto)
+    val existing = attendanceRepository.findByPrisonIdAndBookingIdAndEventIdAndEventDateAndPeriod(
+        provisionalAttendance.prisonId,
+        provisionalAttendance.bookingId,
+        provisionalAttendance.eventId,
+        provisionalAttendance.eventDate,
+        provisionalAttendance.period)
+
+    if (existing.size > 0) {
+      log.info("Attendance already created")
+      throw AttendanceExists()
+    }
+
+    val attendance = attendanceRepository.save(toAttendance(attendanceDto))
+
+    postNomisAttendance(attendance)
+    iepWarningService.postIEPWarningIfRequired(
+        attendance.bookingId,
+        attendance.caseNoteId,
+        attendance.absentReason,
+        attendance.comments,
+        attendance.eventDate
+    ).ifPresent { caseNoteId: Long? -> attendance.caseNoteId = caseNoteId }
+    log.info("attendance created {}", attendance.toBuilder().comments(null))
+    return toAttendanceDto(attendance)
+  }
+
+  @Transactional
+  @Throws(AttendanceNotFound::class, AttendanceLocked::class)
+  open fun updateAttendance(id: Long, newAttendanceDetails: UpdateAttendanceDto) {
+    val attendance = attendanceRepository.findById(id).orElseThrow { AttendanceNotFound() }
+    if (isAttendanceLocked(attendance)) {
+      log.info("Update attempted on locked attendance, attendance id {}", id)
+      throw AttendanceLocked()
+    }
+
+    iepWarningService.handleIEPWarningScenarios(attendance, newAttendanceDetails)
+        .ifPresent { caseNoteId: Long? -> attendance.caseNoteId = caseNoteId }
+
+    attendance.comments = newAttendanceDetails.comments
+    attendance.attended = newAttendanceDetails.attended
+    attendance.paid = newAttendanceDetails.paid
+    attendance.absentReason = newAttendanceDetails.absentReason
+
+    attendanceRepository.save(attendance)
+    postNomisAttendance(attendance)
+  }
+
+  @Transactional
+  open fun attendAll(attendAll: AttendAllDto): Set<AttendanceDto> {
+    val eventOutcome = nomisEventOutcomeMapper.getEventOutcome(null, true, true, "")
+
+    elite2ApiService.putAttendanceForMultipleBookings(attendAll.bookingActivities, eventOutcome)
+
+    val attendances = attendAll.bookingActivities
+        .stream()
+        .map { (bookingId, activityId) ->
+          Attendance.builder()
+              .attended(true)
+              .paid(true)
+              .bookingId(bookingId)
+              .eventId(activityId)
+              .eventDate(attendAll.eventDate)
+              .eventLocationId(attendAll.eventLocationId)
+              .period(attendAll.period)
+              .prisonId(attendAll.prisonId)
+              .build()
+        }
+        .collect(Collectors.toSet())
+
+    attendanceRepository.saveAll(attendances)
+
+    return attendances
+        .stream()
+        .map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }
+        .collect(Collectors.toSet())
+  }
+
+  private fun postNomisAttendance(attendance: Attendance) {
+    val eventOutcome = nomisEventOutcomeMapper.getEventOutcome(
+        attendance.absentReason,
+        attendance.attended,
+        attendance.paid,
+        attendance.comments)
+
+    log.info("Updating attendance on NOMIS {} {}", attendance.toBuilder().comments(null).build(), eventOutcome)
+    elite2ApiService.putAttendance(attendance.bookingId, attendance.eventId, eventOutcome)
+  }
+
+  private fun isAttendanceLocked(attendance: Attendance): Boolean {
+    if (attendance.createDateTime == null) return false
+    val dateOfChange = if (attendance.modifyDateTime == null) attendance.createDateTime.toLocalDate() else attendance.modifyDateTime.toLocalDate()
+    val dateDifference = ChronoUnit.DAYS.between(dateOfChange, LocalDate.now())
+    return if (attendance.paid) dateDifference >= 1 else dateDifference >= 7
+  }
+
+  open fun getAttendanceForOffendersThatHaveScheduledActivity(prisonId: String?, date: LocalDate?, period: TimePeriod?): Set<AttendanceDto> {
+    val bookingIds = elite2ApiService.getBookingIdsForScheduleActivities(prisonId, date, period)
+    val attendances = attendanceRepository.findByPrisonIdAndBookingIdInAndEventDateAndPeriod(prisonId, bookingIds, date, period)
+    return attendances.stream().map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }.collect(Collectors.toSet())
+  }
+
+  @Transactional
+  open fun createAttendances(attendancesDto: AttendancesDto): Set<AttendanceDto> {
+    val attendances = attendancesDto.bookingActivities
+        .stream()
+        .map { (bookingId, activityId) ->
+          Attendance.builder()
+              .bookingId(bookingId)
+              .eventId(activityId)
+              .attended(attendancesDto.attended)
+              .paid(attendancesDto.paid)
+              .absentReason(attendancesDto.reason)
+              .comments(attendancesDto.comments)
+              .eventDate(attendancesDto.eventDate)
+              .eventLocationId(attendancesDto.eventLocationId)
+              .period(attendancesDto.period)
+              .prisonId(attendancesDto.prisonId)
+              .build()
+        }
+        .collect(Collectors.toSet())
+
+    attendanceRepository.saveAll(attendances)
+
+    val eventOutcome = nomisEventOutcomeMapper.getEventOutcome(
+        attendancesDto.reason,
+        attendancesDto.attended,
+        attendancesDto.paid,
+        attendancesDto.comments)
+
+    elite2ApiService.putAttendanceForMultipleBookings(attendancesDto.bookingActivities, eventOutcome)
+
+    return attendances
+        .stream()
+        .map { attendanceData: Attendance -> toAttendanceDto(attendanceData) }
+        .collect(Collectors.toSet())
+  }
+
+  open fun getAbsencesForReason(prisonId: String?, absentReason: AbsentReason?, fromDate: LocalDate, toDate: LocalDate?,
+                           period: TimePeriod?): Set<AbsenceDto> {
+
+    val periods = if (period == null) setOf(TimePeriod.AM, TimePeriod.PM) else setOf(period)
+    val endDate = toDate ?: fromDate
+
+    val offenderDetails = periods
+        .stream()
+        .flatMap { p: TimePeriod ->
+          elite2ApiService.getScheduleActivityOffenderData(prisonId, fromDate, endDate, p)
+              .stream().map { offender: OffenderDetails -> offenderDetailsWithPeriod(offender, p) }
+        }
+        .collect(Collectors.toList())
+
+    val attendances = attendanceRepository
+        .findByPrisonIdAndEventDateBetweenAndPeriodInAndAbsentReason(prisonId, fromDate, endDate, periods, absentReason)
+
+    return attendances.stream()
+        .filter { attendance: Attendance -> offenderDetails.stream().anyMatch(findAttendance(attendance.bookingId, attendance.eventId, attendance.period)) }
+        .map { attendance: Attendance ->
+          val details = offenderDetails.stream()
+              .filter(findAttendance(attendance.bookingId, attendance.eventId, attendance.period)).findFirst()
+              .orElseThrow()
+          toAbsenceDto(details, attendance)
+        }
+        .collect(Collectors.toSet())
+  }
+
+  private fun offenderDetailsWithPeriod(details: OffenderDetails, period: TimePeriod): OffenderDetails {
+    return details.copy(
+        details.bookingId,
+        details.offenderNo,
+        details.eventId,
+        details.cellLocation,
+        details.eventDate,
+        period.toString(),
+        details.firstName,
+        details.lastName,
+        details.comment
+    )
+  }
+
+  private fun toAbsenceDto(details: OffenderDetails, attendance: Attendance): AbsenceDto {
+    return AbsenceDto(
+        attendance.id,
+        attendance.bookingId,
+        details.offenderNo,
+        attendance.eventId,
+        attendance.eventLocationId,
+        attendance.eventDate,
+        TimePeriod.valueOf(details.timeSlot!!),
+        attendance.absentReason,
+        details.comment,
+        attendance.comments,
+        details.cellLocation,
+        details.firstName,
+        details.lastName
+    )
+  }
+
+  private fun findAttendance(bookingId: Long, eventId: Long?, period: TimePeriod): Predicate<in OffenderDetails> {
+    return Predicate { (bookingId1, _, eventId1, _, _, timeSlot) ->
+      bookingId1 == bookingId &&
+          eventId1 == eventId && TimePeriod.valueOf(timeSlot!!) == period
+    }
+  }
+
+  private fun toAttendance(attendanceDto: CreateAttendanceDto): Attendance {
+    return Attendance
+        .builder()
+        .eventLocationId(attendanceDto.eventLocationId)
+        .eventDate(attendanceDto.eventDate)
+        .eventId(attendanceDto.eventId)
+        .bookingId(attendanceDto.bookingId)
+        .period(attendanceDto.period)
+        .paid(attendanceDto.paid)
+        .attended(attendanceDto.attended)
+        .prisonId(attendanceDto.prisonId)
+        .absentReason(attendanceDto.absentReason)
+        .comments(attendanceDto.comments)
+        .build()
+  }
+
+  private fun toAttendanceDto(attendanceData: Attendance): AttendanceDto {
+    return AttendanceDto.builder()
+        .id(attendanceData.id)
+        .eventDate(attendanceData.eventDate)
+        .eventId(attendanceData.eventId)
+        .bookingId(attendanceData.bookingId)
+        .period(attendanceData.period)
+        .paid(attendanceData.paid)
+        .attended(attendanceData.attended)
+        .prisonId(attendanceData.prisonId)
+        .absentReason(attendanceData.absentReason)
+        .eventLocationId(attendanceData.eventLocationId)
+        .comments(attendanceData.comments)
+        .createUserId(attendanceData.createUserId)
+        .createDateTime(attendanceData.createDateTime)
+        .caseNoteId(attendanceData.caseNoteId)
+        .locked(isAttendanceLocked(attendanceData))
+        .modifyDateTime(attendanceData.modifyDateTime)
+        .modifyUserId(attendanceData.modifyUserId)
+        .build()
+  }
+}
