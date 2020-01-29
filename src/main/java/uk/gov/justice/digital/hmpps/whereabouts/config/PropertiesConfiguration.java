4e168f29e7d1311b438029691f4678b878c59b04
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-01-29 16:08:39 +0000

    DT-256 Move tests associated with moved endpoint /agencies/{agencyId}/locations/groups

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/PropertiesConfiguration.java
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/PropertiesConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
+@Configuration
 public class PropertiesConfiguration {
 
     @Value("classpath:whereabouts/patterns/*.properties")
