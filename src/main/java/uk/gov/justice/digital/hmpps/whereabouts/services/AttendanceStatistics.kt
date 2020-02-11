e02647132444b76ca0ec50e410fc0a915a08b62a
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-02-11 14:05:04 +0000

    Nn 2410 fix unaccounted for am pm bug (#124)
    * NN-2410: Add test and fix bug when calculating unaccounted for for AM + PM
    
    * NN-2410: Minor style adjustment
    
    * NN-2410: Move not recorded logic to separate function

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/AttendanceStatistics.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/AttendanceStatistics.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.whereabouts.services
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.whereabouts.model.AbsentReason
+import uk.gov.justice.digital.hmpps.whereabouts.model.Attendance
 import uk.gov.justice.digital.hmpps.whereabouts.model.TimePeriod
 import uk.gov.justice.digital.hmpps.whereabouts.repository.AttendanceRepository
 import java.time.LocalDate
@@ -22,11 +23,9 @@ open class AttendanceStatistics(private val attendanceRepository: AttendanceRepo
       else -> attendanceRepository.findByPrisonIdAndPeriodAndEventDateBetween(prisonId, period, from, to)
     }
 
-    val attendanceBookingIds = attendances.map { it.bookingId }
-
     return Stats(
         scheduleActivities = offendersScheduledForActivity.count(),
-        notRecorded = offendersScheduledForActivity.count { !attendanceBookingIds.contains(it) },
+        notRecorded = calculateNotRecorded(offendersScheduledForActivity, attendances),
         paidReasons = PaidReasons(
             attended = attendances.count { it.attended },
             acceptableAbsence = attendances.count { it.absentReason == AbsentReason.AcceptableAbsence },
@@ -45,4 +44,24 @@ open class AttendanceStatistics(private val attendanceRepository: AttendanceRepo
 
   private fun getScheduleActivityForPeriods(prisonId: String, from: LocalDate, to: LocalDate, periods: Set<TimePeriod>): List<Long> =
       periods.map { elite2ApiService.getBookingIdsForScheduleActivitiesByDateRange(prisonId, it, from, to) }.flatten()
+
+  private fun calculateNotRecorded(scheduledBookingIds: List<Long>, attendedBookingIds: Set<Attendance>): Int {
+    // This creates a Grouping that looks like {1=2, 2=2}
+    // Where the key is the booking id and the value is the
+    // number of times that booking id appears
+    val scheduledBookingIdsCount = scheduledBookingIds.groupingBy { it }.eachCount()
+    val attendancesBookingIdsCount = attendedBookingIds.groupingBy { it.bookingId }.eachCount()
+
+    // Iterate over the scheduled booking ids grouping
+    // and check whether the number of times that booking
+    // id appears in the schedules matches the number of times
+    // it appears in the attendances. If not, the difference
+    // is added
+    return scheduledBookingIdsCount.keys.map {
+      if (!attendancesBookingIdsCount.containsKey(it))
+        scheduledBookingIdsCount.getValue(it)
+      else
+        scheduledBookingIdsCount.getValue(it) - attendancesBookingIdsCount.getValue(it)
+    }.fold(0) { acc, current -> acc + current}
+  }
 }
