837f70de9cee846e1acb65ffed86e0f3f9773d11
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2020-12-03 09:26:02 +0000

    NN-3081 undo dps gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.1.1"
   kotlin("plugin.spring") version "1.4.10"
   kotlin("plugin.jpa") version "1.4.10"
   id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
