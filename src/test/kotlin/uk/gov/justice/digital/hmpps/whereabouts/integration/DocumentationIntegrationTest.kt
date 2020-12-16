9bf4ac273188024b642378dcd4304ea37c13a803
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-12-16 23:40:03 +0000

    DCS-748 Moving over to OpenAPI and fixing swagger v2 api doc

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/integration/DocumentationIntegrationTest.kt
@@ -0,0 +1,39 @@
+package uk.gov.justice.digital.hmpps.whereabouts.integration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.util.MimeType
+
+@ExtendWith(SpringExtension::class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DocumentationIntegrationTest {
+  @Autowired
+  lateinit var testRestTemplate: TestRestTemplate
+  @Autowired
+  lateinit var mapper: ObjectMapper
+
+  @Test
+  fun `swagger 2 generates valid json`() {
+    val response = testRestTemplate.getForEntity("/v2/api-docs", String::class.java)
+
+    assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    mapper.readTree(response.body)
+  }
+
+  @Test
+  fun `openApi generates valid json`() {
+    val response = testRestTemplate.getForEntity("/v3/api-docs", String::class.java)
+
+    assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    mapper.readTree(response.body)
+  }
+}
