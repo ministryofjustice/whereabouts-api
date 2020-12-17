7736b558610704cb32f63b857d427ec0e97249d5
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-12-17 00:25:49 +0000

    DCS-748 Fix formatting

--- src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/integration/DocumentationIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/integration/DocumentationIntegrationTest.kt
@@ -10,7 +10,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.util.MimeType
 
 @ExtendWith(SpringExtension::class)
 @ActiveProfiles("test")
