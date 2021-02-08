9e100c70cb81c0b162aec905741b25d2e5249a29
Peter Phillips <peter@greenthistle.co.uk>
2021-02-08 12:10:42 +0000

    DT-1597: ⬆️ Suppress owasp vuln and upgrade to kotlin 1.4.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.0"
   kotlin("plugin.spring") version "1.4.21"
   kotlin("plugin.jpa") version "1.4.21"
 }
