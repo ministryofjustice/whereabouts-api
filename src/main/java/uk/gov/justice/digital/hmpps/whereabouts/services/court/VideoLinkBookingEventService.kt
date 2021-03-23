606cb388fe00046ea0c631300dff34503b8516c1
bell-pa <peter.bell@digital.justice.gov.uk>
2021-03-23 17:39:42 +0000

    DCS-888 Book a Video Link - Analytics

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/court/VideoLinkBookingEventService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/court/VideoLinkBookingEventService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.whereabouts.services.court
 
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.whereabouts.repository.VideoLinkBookingEventRepository
@@ -13,7 +12,6 @@ class VideoLinkBookingEventService(
   val csvConverter: EventToCsvConverter
 ) {
   @Transactional(readOnly = true)
-  @PreAuthorize("hasAnyRole('ROLE_VIDEO_LINK_COURT_USER')")
   fun getEventsAsCSV(startDate: LocalDate, days: Long): String =
     csvConverter.toCsv(repository.findByDatesBetween(startDate, startDate.plusDays(days)))
 }
