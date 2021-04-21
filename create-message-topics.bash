264fee749da564d0c2c7dc521f27f3e53d0ef5bf
petergphillips <peter@greenthistle.co.uk>
2021-04-21 16:02:08 +0100

    DT-1949: ⬆️ Update to latest localstack (#282)

--- create-message-topics.bash
+++ create-message-topics.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws --endpoint-url=http://localhost:4575 sns publish --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"DATA_COMPLIANCE_DELETE-OFFENDER"}}' --message '{"offenderIdDisplay":"A1234AA"}'
+aws --endpoint-url=http://localhost:4566 sns publish --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"DATA_COMPLIANCE_DELETE-OFFENDER"}}' --message '{"offenderIdDisplay":"A1234AA"}'
