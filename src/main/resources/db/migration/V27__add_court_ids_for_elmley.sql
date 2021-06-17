2f404080f0877b31b06b75f2861c50fec7aa8270
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-06-17 09:40:00 +0100

    DCS-1031 Extracting new courts into new migration

---
+++ src/main/resources/db/migration/V27__add_court_ids_for_elmley.sql
@@ -0,0 +1,14 @@
+
+insert into enabled_court (id, name) values
+(  'CANTCC', 'Canterbury Crown'),                   -- {"id":"CANTCC","name":"Canterbury Crown Court"}
+(  'CANTCT', 'Canterbury County'),                  -- {"id":"CANTCT","name":"Canterbury County Court"}
+(  'CANTMC', 'Canterbury Magistrates'),             -- {"id":"CANTMC","name":"Canterbury Magistrates Court"}
+(  'FLKSMC', 'Folkestone Magistrates'),             -- {"id":"FLKSMC","name":"Folkestone Magistrates Court"}
+(  'MDSTCC', 'Maidstone Crown'),                    -- {"id":"MDSTCC","name":"Maidstone Crown Court"}
+(  'MDSTCT', 'Maidstone County'),                   -- {"id":"MDSTCT","name":"Maidstone County Court"}
+(  'MDSTMC', 'Maidstone Magistrates'),              -- {"id":"MDSTMC","name":"Maidstone Magistrates Court"}
+(  'MRGTMC', 'Margate Magistrates'),                -- {"id":"MRGTMC","name":"Margate Magistrates Court"}
+(  'CHTHMC', 'Medway Magistrates'),                 -- {"id":"CHTHMC","name":"Medway Magistrates Court"}
+(  'MDWYCT', 'Medway County'),                      -- {"id":"MDWYCT","name":"Medway County Court"}
+(  'SEVNMC', 'Sevenoaks Magistrates');              -- {"id":"SEVNMC","name":"Sevenoaks Magistrates Court"}
+
