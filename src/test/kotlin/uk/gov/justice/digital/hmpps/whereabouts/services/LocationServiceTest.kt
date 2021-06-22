0b40189dc829f9e22425c27214088cecb64d4325
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-06-22 16:36:03 +0100

    DCS-1042 new endpoint for getting rooms in prison (#327)
    The rooms must have an eventType of 'APP' and a locationType of 'VIDE'

--- src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/services/LocationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/services/LocationServiceTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyString
 import uk.gov.justice.digital.hmpps.whereabouts.model.CellWithAttributes
 import uk.gov.justice.digital.hmpps.whereabouts.model.Location
+import uk.gov.justice.digital.hmpps.whereabouts.services.locationfinder.LocationIdAndDescription
 import java.util.Properties
 import java.util.function.Predicate
 import javax.persistence.EntityNotFoundException
@@ -24,6 +25,39 @@ class LocationServiceTest {
   private val cell2 = aLocation(locationPrefix = "cell2")
   private val cell3 = aLocation(locationPrefix = "cell3")
   private val cell4 = aLocation(locationPrefix = "cell4")
+
+  @Test
+  fun `getVideoLinkRoomsForPrison - filter by type then return Ids and descriptions`() {
+    val location1 = Location(
+      locationId = 1, locationType = "VIDE", description = "Video Room A", locationUsage = "APP",
+      agencyId = "MDI", parentLocationId = 123, currentOccupancy = 2, locationPrefix = "MDI-prefix",
+      operationalCapacity = 2, userDescription = null, internalLocationCode = "Room 1"
+    )
+
+    val location2 = Location(
+      locationId = 2, locationType = "VIDE", description = "Video Room B", locationUsage = "APP",
+      agencyId = "MDI", parentLocationId = 123, currentOccupancy = 2, locationPrefix = "MDI-prefix",
+      operationalCapacity = 2, userDescription = null, internalLocationCode = "Room 2"
+    )
+
+    val location3 = Location(
+      locationId = 3, locationType = "MEETING ROOM", description = "Video Room C", locationUsage = "APP",
+      agencyId = "MDI", parentLocationId = 123, currentOccupancy = 2, locationPrefix = "MDI-prefix",
+      operationalCapacity = 2, userDescription = null, internalLocationCode = "Room 3"
+    )
+
+    whenever(prisonApiService.getAgencyLocationsForTypeUnrestricted("MDI", "APP"))
+      .thenReturn(listOf(location1, location2, location3))
+
+    assertThat(locationService.getVideoLinkRoomsForPrison("MDI"))
+      .isEqualTo(
+        listOf(
+          LocationIdAndDescription(locationId = 1, description = "Video Room A"),
+          LocationIdAndDescription(locationId = 2, description = "Video Room B"),
+        )
+      )
+      .doesNotContain(LocationIdAndDescription(locationId = 3, description = "Video Room C"))
+  }
 
   @Test
   fun `getCellLocationsForGroup - cells match predicate - returns cells`() {
