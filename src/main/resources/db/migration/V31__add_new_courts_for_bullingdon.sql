de8456a7b6cf6e9003093705e12d4ce6479ee848
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-07-19 16:26:05 +0100

    DCS-1072 Adding new courts for Bullingdon

---
+++ src/main/resources/db/migration/V31__add_new_courts_for_bullingdon.sql
@@ -0,0 +1,13 @@
+
+insert into enabled_court (id, name) values
+(  'OXFDCC', 'Oxford Crown'),
+(  'OXFRMC', 'Oxfordshire Magistrates'),
+(  'READCC', 'Reading Crown'),
+(  'READMC', 'Reading Magistrates'),
+(  'SLOUMC', 'Slough Magistrates'),
+(  'SWINCC', 'Swindon Crown'),
+(  'SWINMC', 'Swindon Magistrates'),
+(  'AMERCC', 'Amersham Crown'),
+(  'AYLSCC', 'Aylesbury Crown'),
+(  'WYCBMC', 'High Wycombe Magistrates'),
+(  'MLTKMC', 'Milton Keynes Magistrates');
