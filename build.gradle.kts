ac41e96e0447dd943e35de59fbbc5fb7925b1ee6
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-09-15 15:57:33 +0100

    NN-3646 bump dps plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.8"
   kotlin("plugin.spring") version "1.5.20"
-  kotlin("plugin.jpa") version "1.5.20"
+  kotlin("plugin.jpa") version "1.5.30"
 }
 
 configurations {
