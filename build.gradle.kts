a9e667c5167cf76df60177aa504525e2ca09ed10
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-10-04 12:49:41 +0100

    NN-3693 upping dependency to get rid of security issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
   kotlin("plugin.spring") version "1.5.30"
   kotlin("plugin.jpa") version "1.5.30"
 }
