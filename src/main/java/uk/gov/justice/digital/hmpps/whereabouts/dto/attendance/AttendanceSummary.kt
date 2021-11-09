994d28ba8039a475c884efeb19082c845f2b62d2
steverendell <steve.rendell@syscon.net>
2021-11-09 16:18:16 +0000

    ESWE-202 Unacceptable absences for the last 30 days

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/attendance/AttendanceSummary.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/dto/attendance/AttendanceSummary.kt
@@ -6,8 +6,6 @@ import java.time.YearMonth
 
 @ApiModel(description = "Attendances aggregate data")
 data class AttendanceSummary(
-  @ApiModelProperty(value = "Period")
-  val month: YearMonth,
   @ApiModelProperty(value = "Number of acceptable absences in period")
   var acceptableAbsence: Int = 0,
   @ApiModelProperty(value = "Number of unacceptable absences in period")
