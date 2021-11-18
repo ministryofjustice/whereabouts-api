db707a5394fcc219d3d12040891bb952d63e70df
Louise Norris <louise.norris@digital.justice.gov.uk>
2021-11-18 13:19:51 +0000

    DCS-1295 add additional courts for peterborough

---
+++ src/main/resources/db/migration/V32__add_new_courts_for_peterborough.sql
@@ -0,0 +1,10 @@
+
+insert into enabled_court (id, name) values
+(  'CAMBCT', 'Cambridge Crown'),
+(  'CAMBMC', 'Cambridge Magistrates'),
+(  'HUNTMC', 'Huntingdon Law Courts'),
+(  'PBORCC', 'Peterborough Crown'),
+(  'PBORMC', 'Peterborough Magistrates'),
+(  'NRTHCC', 'Northampton Crown'),
+(  'NRTHMC', 'Northampton Magistrates'),
+(  'WELBMC', 'Wellingborough Justice Centre');
