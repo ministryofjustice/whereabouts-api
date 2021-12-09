2b790fbc74c6f7757740e7ac34c047d9bfb08eea
Louise Norris <louise.norris@digital.justice.gov.uk>
2021-12-09 09:57:44 +0000

    DCS-1342 update courts for hewell

---
+++ src/main/resources/db/migration/V33__update_courts_for_hewell.sql
@@ -0,0 +1,6 @@
+
+insert into enabled_court (id, name) values
+(  'BRMNCC', 'Birmingham Crown'),
+(  'BRMCMC', 'Birmingham Magistrates');
+
+DELETE FROM enabled_court WHERE name='Shrewsbury Magistrates';
