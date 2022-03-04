1649960c3ec13b5c0869f35f826d77a2c97824a7
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2022-03-04 15:46:01 +0000

    DCS-1469: ✨ Add new courts for Wandsworth

---
+++ src/main/resources/db/migration/V34__add_new_courts_for_wandsworth.sql
@@ -0,0 +1,7 @@
+
+insert into enabled_court (id, name) values
+(  'CAMBCT', 'Guildford Crown'),
+(  'GLDFMC', 'Guildford Magistrates'),
+(  'STAIMC', 'Staines Magistrates'),
+(  'ISLWCC', 'Isleworth Crown'),
+(  'SNARCC', 'Snaresbrook Crown'),
