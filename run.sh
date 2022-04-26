372e874088ce7f128c0c911d6499f90beceb74b4
sp-watson <77974320+sp-watson@users.noreply.github.com>
2022-04-26 08:55:40 +0100

    NN-4050: Change to prod-ready garbage collector (#433)

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 exec java ${JAVA_OPTS} \
+  -XX:+AlwaysActAsServerClassMachine \
   -Dcom.sun.management.jmxremote.local.only=false \
   -Djava.security.egd=file:/dev/./urandom \
   -javaagent:/app/agent.jar \
