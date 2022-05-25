ef9d0083c287ef0474c21b9b859e24d00470393f
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2022-05-25 11:22:05 +0100

    DCS-1631 new migration file for Norwich area courts

---
+++ src/main/resources/db/migration/V43_add_new_courts_for_norwich.sql
@@ -0,0 +1,9 @@
+insert into enabled_court (id, name) values
+(  'NORWCC', 'Norwich Crown'),
+(  'NORWMC', 'Norwich Magistrates'),
+(  'NORWCT', 'Norwich County'),
+(  'IPSWCC', 'Ipswich Crown'),
+(  'IPSWMC', 'Ipswich Magistrates'),
+(  'KNGSCC', 'Kings Lynn Crown'),
+(  'KNGSMC', 'Kings Lynn Magistrates'),
+(  'GRTYMC', 'Great Yarmouth Magistrates');
