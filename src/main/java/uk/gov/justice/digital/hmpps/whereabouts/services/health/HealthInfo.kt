75000902e50820040e1f55f067cabe926ac6db07
petergphillips <peter@greenthistle.co.uk>
2022-06-06 12:23:53 +0100

    SDI-214: 🔇 Remove unnecessary log line and convert to kotlin (#451)

---
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/health/HealthInfo.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.whereabouts.services.health
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.info.BuildProperties
+import org.springframework.stereotype.Component
+
+/**
+ * Adds version data to the /health endpoint. This is called by the UI to display API details
+ */
+@Component
+class HealthInfo(@Autowired private val buildProperties: BuildProperties) : HealthIndicator {
+  private val version: String = buildProperties.version
+
+  override fun health(): Health = Health.up().withDetail("version", version).build()
+}
