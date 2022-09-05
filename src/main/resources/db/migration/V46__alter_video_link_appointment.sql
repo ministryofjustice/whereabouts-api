628dc26627ed41d187805c9b410bec5d2234b98b
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-09-05 09:12:08 +0100

    DCS-1733 fixing sql

--- src/main/resources/db/migration/V46__alter_video_link_appointment.sql
+++ src/main/resources/db/migration/V46__alter_video_link_appointment.sql
@@ -1,3 +1,1 @@
-ALTER TABLE VIDEO_LINK_APPOINTMENT
-    ADD start_date_time TIMESTAMP,
-    ADD start_date_time TIMESTAMP;
+ALTER TABLE VIDEO_LINK_APPOINTMENT ADD (start_date_time TIMESTAMP, end_date_time TIMESTAMP);
