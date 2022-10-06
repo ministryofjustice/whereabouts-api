e0e0d79eb0b49001e44ba9a8231f9bc504a16601
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-10-06 21:34:52 +0100

    DCS-1736 Provide additional debug info to migration endpoint

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/controllers/VideoLinkAppointmentMigrationResponse.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/controllers/VideoLinkAppointmentMigrationResponse.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.whereabouts.controllers
 
 import io.swagger.annotations.ApiModel
+import uk.gov.justice.digital.hmpps.whereabouts.services.VideoLinkBookingMigrationService.Outcome
 
 @ApiModel(description = "Information about booking migration ")
 data class VideoLinkAppointmentMigrationResponse(
   val videoLinkAppointmentRemaining: Long,
   val videoLinkBookingRemaining: Long,
+  val outcomes: Map<Outcome, Int>,
 )
--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/VideoLinkBookingMigrationService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/VideoLinkBookingMigrationService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.whereabouts.services
 
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,6 +11,10 @@ import uk.gov.justice.digital.hmpps.whereabouts.model.VideoLinkAppointment
 import uk.gov.justice.digital.hmpps.whereabouts.model.VideoLinkBooking
 import uk.gov.justice.digital.hmpps.whereabouts.repository.VideoLinkAppointmentRepository
 import uk.gov.justice.digital.hmpps.whereabouts.repository.VideoLinkBookingRepository
+import uk.gov.justice.digital.hmpps.whereabouts.services.VideoLinkBookingMigrationService.Outcome.BOOKING_DELETED
+import uk.gov.justice.digital.hmpps.whereabouts.services.VideoLinkBookingMigrationService.Outcome.MIGRATED_NO_MODIFICATIONS
+import uk.gov.justice.digital.hmpps.whereabouts.services.VideoLinkBookingMigrationService.Outcome.POST_DELETED
+import uk.gov.justice.digital.hmpps.whereabouts.services.VideoLinkBookingMigrationService.Outcome.PRE_DELETED
 
 @Service
 @Transactional
@@ -19,42 +24,52 @@ class VideoLinkBookingMigrationService(
   val videoLinkAppointmentRepository: VideoLinkAppointmentRepository,
   val prisonApiService: PrisonApiService,
 ) {
+  enum class Outcome { MIGRATED_NO_MODIFICATIONS, BOOKING_DELETED, PRE_DELETED, POST_DELETED }
+
   fun migrateFromNomis(batchSize: Int): VideoLinkAppointmentMigrationResponse {
 
-    videoLinkBookingRepository.findByPrisonIdIsNull(PageRequest.of(0, batchSize)).stream()
-      .forEach { v -> updateVideoLink(v) }
+    val outcomes = videoLinkBookingRepository.findByPrisonIdIsNull(PageRequest.of(0, batchSize))
+      .flatMap { v -> updateVideoLink(v) }
 
     return VideoLinkAppointmentMigrationResponse(
       videoLinkAppointmentRepository.countByLocationIdIsNull(),
       videoLinkBookingRepository.countByPrisonIdIsNull(),
+      outcomes.groupingBy { it }.eachCount()
     )
   }
 
-  fun updateVideoLink(videoLinkBooking: VideoLinkBooking) {
+  fun updateVideoLink(videoLinkBooking: VideoLinkBooking): List<Outcome> {
     val mainAppointment = videoLinkBooking.appointments[HearingType.MAIN]
     val preAppointment = videoLinkBooking.appointments[HearingType.PRE]
     val postAppointment = videoLinkBooking.appointments[HearingType.POST]
 
+    val outcomes = mutableListOf<Outcome>()
     val nomisMainAppointment = updateAppointment(mainAppointment!!)
     if (nomisMainAppointment == null) {
+      log.info("Deleting booking: ${videoLinkBooking.id}")
       videoLinkBookingRepository.delete(videoLinkBooking)
+      outcomes.add(BOOKING_DELETED)
     } else {
       videoLinkBooking.prisonId = nomisMainAppointment.agencyId
       videoLinkBooking.comment = nomisMainAppointment.comment
 
       if (preAppointment?.let { updateAppointment(it) } == null) {
         videoLinkBooking.appointments.remove(HearingType.PRE)
+        outcomes.add(PRE_DELETED)
       }
       if (postAppointment?.let { updateAppointment(it) } == null) {
         videoLinkBooking.appointments.remove(HearingType.POST)
+        outcomes.add(POST_DELETED)
       }
       videoLinkBookingRepository.save(videoLinkBooking)
     }
+    return if (outcomes.isEmpty()) listOf(MIGRATED_NO_MODIFICATIONS) else outcomes
   }
 
   private fun updateAppointment(appointment: VideoLinkAppointment): PrisonAppointment? {
     val nomisAppointment = prisonApiService.getPrisonAppointment(appointment.appointmentId)
     if (nomisAppointment == null) {
+      log.info("Deleting appointment: ${appointment.appointmentId}")
       return null
     } else {
       appointment.startDateTime = nomisAppointment.startTime
@@ -62,5 +77,9 @@ class VideoLinkBookingMigrationService(
       appointment.locationId = nomisAppointment.eventLocationId
       return nomisAppointment
     }
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
   }
 }
