533e2994d9cb5aab475b1ee1b217b8a46e260c39
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-10-19 12:26:46 +0100

    DCS-1819 Fixing issue when deleting appointments and no user token available
    This matches the pattern used by the attendance service

---
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/VideoLinkBookingServiceForAppScope.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.whereabouts.config
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.whereabouts.repository.VideoLinkAppointmentRepository
+import uk.gov.justice.digital.hmpps.whereabouts.repository.VideoLinkBookingRepository
+import uk.gov.justice.digital.hmpps.whereabouts.services.PrisonApiService
+import uk.gov.justice.digital.hmpps.whereabouts.services.PrisonApiServiceAuditable
+import uk.gov.justice.digital.hmpps.whereabouts.services.court.CourtService
+import uk.gov.justice.digital.hmpps.whereabouts.services.court.VideoLinkBookingEventListener
+import uk.gov.justice.digital.hmpps.whereabouts.services.court.VideoLinkBookingService
+import java.time.Clock
+
+@Configuration
+class VideoLinkBookingServiceForAppScope {
+  @Bean(name = ["videoLinkBookingServiceAppScope"])
+  fun getPrisonServiceForAppScope(
+    courtService: CourtService,
+    @Qualifier("elite2WebClientAppScope") webClient: WebClient,
+    prisonApiServiceAuditable: PrisonApiServiceAuditable,
+    videoLinkAppointmentRepository: VideoLinkAppointmentRepository,
+    videoLinkBookingRepository: VideoLinkBookingRepository,
+    clock: Clock,
+    videoLinkBookingEventListener: VideoLinkBookingEventListener
+  ): VideoLinkBookingService {
+    return VideoLinkBookingService(
+      courtService,
+      PrisonApiService(webClient),
+      prisonApiServiceAuditable,
+      videoLinkAppointmentRepository,
+      videoLinkBookingRepository,
+      clock,
+      videoLinkBookingEventListener
+    )
+  }
+}
