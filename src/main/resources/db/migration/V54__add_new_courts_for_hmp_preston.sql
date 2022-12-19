9af29bc2397c9cdba95029b154bdf47d1f6f0b76
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2022-12-19 16:08:51 +0000

    DCS-1919 adding new courts (#525)
    * DCS-1919 adding new courts

---
+++ src/main/resources/db/migration/V54__add_new_courts_for_hmp_preston.sql
@@ -0,0 +1,11 @@
+insert into enabled_court (id, name) values
+(  'CRT022', 'Workington Magistrates'),
+(  'BRRWMC', 'Barrow Magistrates'),
+(  'LANCMC', 'Lancaster Magistrates'),
+(  'BURNMC', 'Burnley Magistrates'),
+(  'BURNCC', 'Burnley Crown'),
+(  'BLKBMC', 'Blackburn Magistrates'),
+(  'PRESCC', 'Preston Crown Ringway'),
+(  'PRESCT', 'Preston Crown Sessions House'),
+(  'PRESMC', 'Preston Magistrates'),
+(  'BLKPMC', 'Blackpool Magistrates');
