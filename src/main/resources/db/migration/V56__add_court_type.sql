4752cb73fd41a204f0a3726bea6bf4c311d0036d
Louise Norris <louise.norris@digital.justice.gov.uk>
2023-02-13 14:02:38 +0000

    DCS-2013 add court type table and values for VLBs

---
+++ src/main/resources/db/migration/V56__add_court_type.sql
@@ -0,0 +1,31 @@
+create table court_type (
+    id varchar(20) not null primary key,
+    name varchar(50) not null
+);
+
+insert into court_type (id, name) values
+(  'APPEAL', 'Appeal'),
+(  'APPLICATION', 'Application'),
+(  'BACKER_TRIAL', 'Backer Trial'),
+(  'BAIL', 'Bail'),
+(  'CIVIL', 'Civil'),
+(  'COMMITTAL_FOR_SENTENCE', 'Committal for Sentence (CSE)'),
+(  'CUSTODY_TIME_LIMIT_APPLICATIONS', 'Custody Time Limit Applications (CTA)'),
+(  'IMMIGRATION_DEPORTATION', 'Immigration/Deportation'),
+(  'FAMILY', 'Family'),
+(  'TRIAL', 'Trial'),
+(  'FURTHER_CASE_MANAGEMENT', 'Further case Management Hearings (FCMH)'),
+(  'FUTURE_TRIAL_REVIEW', 'Future Trial Review hearing (FTR)'),
+(  'GROUND_RULES', 'Ground Rules Hearing (GRH)'),
+(  'MENTION_DEFENDANT_MUST_ATTEND', 'Mention hearing Defendant must attend (MDA)'),
+(  'MENTION_TO_FIX', 'Mention to Fix (MEF)'),
+(  'NEWTON', 'Newton Hearing'),
+(  'PLEA', 'Plea Hearing (PLE)'),
+(  'PLEA_TRIAL_PREPARATION', 'Plea Trial and Preparation Hearing (PTPH)'),
+(  'PRE_TRIAL_REVIEW', 'Pre-Trial Review (PTR)'),
+(  'PROCEEDS_OF_CRIME_APPLICATIONS', 'Proceeds of Crime Applications (POCA)'),
+(  'REMAND', 'Remand Hearing'),
+(  'SECTION_28', 'Section 28'),
+(  'SENTENCE', 'Sentence (SEN)'),
+(  'TRIBUNAL', 'Tribunal'),
+(  'OTHER', 'Other');
