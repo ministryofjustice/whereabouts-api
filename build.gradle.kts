5656720f28c5eae8738c41fe4f45b538f41a127d
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-09-07 09:19:53 +0100

    NN-5081 Changed the Spring boot version from 5.2..2 to 5.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.0"
   kotlin("plugin.spring") version "1.9.0"
   kotlin("plugin.jpa") version "1.9.0"
 }
