2e7bca3b6eba7f9aff7aa61df48df58887bdeb7a
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2023-12-07 09:20:11 +0000

    MAP-483 adding repository tests

--- src/test/resources/repository/video-link-appointment.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE video_link_appointment RESTART IDENTITY;
-INSERT INTO video_link_appointment (id, appointment_id, hearing_type, video_link_booking_id, location_id, start_date_time, end_date_time) VALUES
-(1, 00000001, 'MAIN', 1, 1292, to_timestamp('2023-01-01 01:00:00', 'YYYY-MM-DD HH:MI:SS'), to_timestamp('2023-01-01 02:00:00', 'YYYY-MM-DD HH:MI:SS')),
-(2, 00000002, 'MAIN', 2, 1293, to_timestamp('2023-01-01 01:00:00', 'YYYY-MM-DD HH:MI:SS'), to_timestamp('2023-01-01 02:00:00', 'YYYY-MM-DD HH:MI:SS'));
