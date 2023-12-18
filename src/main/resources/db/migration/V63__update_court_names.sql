0b5318dfcc6e13d9e02fb0b84276ded75b490d29
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-12-18 11:08:39 +0000

    MAP-506 correct id

--- src/main/resources/db/migration/V63__update_court_names.sql
+++ src/main/resources/db/migration/V63__update_court_names.sql
@@ -1,3 +1,3 @@
 update enabled_court set name = 'Probation - RWF - Snaresbrook CC (PPOC)' where id = 'PPOCCT';
 update enabled_court set name = 'Probation - TWR - Thames&Stratford MC (PPOC)' where id = 'THMSPP';
-update enabled_court set name = 'Probation - HAR - Wood Green CC (PPOC)' where id = 'HGHBPP';
+update enabled_court set name = 'Probation - HAR - Wood Green CC (PPOC)' where id = 'WDGNPP';
