1b630f3dc763dd00243b48041dccafe0927a3d63
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2024-01-16 10:40:59 +0000

    MAP-465 align config naming

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/VideoLinkBookingServiceForAppScope.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/VideoLinkBookingServiceForAppScope.kt
@@ -42,7 +42,7 @@ class VideoLinkBookingServiceForAppScope(
       clock,
       videoLinkBookingEventListener,
       notifyService,
-      prisonRegisterClient,
+      PrisonRegisterClient(webClient),
     )
   }
 }
