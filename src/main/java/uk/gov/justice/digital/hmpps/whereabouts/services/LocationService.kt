5a53aabcee989352404e4a3071ad59302c64b1fd
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2024-08-12 09:19:44 +0100

    MAP-1480 Remove unused /locations/{agencyId}/{group}/location-prefix endpoint (#696)

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/LocationService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/services/LocationService.kt
@@ -1,19 +1,15 @@
 package uk.gov.justice.digital.hmpps.whereabouts.services
 
-import jakarta.persistence.EntityNotFoundException
 import org.apache.commons.text.WordUtils
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.whereabouts.dto.LocationPrefixDto
 import uk.gov.justice.digital.hmpps.whereabouts.model.Location
 import uk.gov.justice.digital.hmpps.whereabouts.model.LocationIdAndDescription
-import java.util.Properties
 
 @Service
 class LocationService(
   private val prisonApiService: PrisonApiService,
   @Qualifier("locationGroupServiceSelector") private val locationGroupService: LocationGroupService,
-  @Qualifier("whereaboutsGroups") private val groupsProperties: Properties,
 ) {
 
   fun getVideoLinkRoomsForPrison(agencyId: String): List<LocationIdAndDescription> =
@@ -26,18 +22,6 @@ class LocationService(
     prisonApiService
       .getAgencyLocationsForTypeUnrestricted(agencyId, "APP")
       .map { LocationIdAndDescription(it.locationId, it.userDescription ?: it.description) }
-
-  fun getLocationPrefixFromGroup(agencyId: String, group: String): LocationPrefixDto {
-    val agencyGroupKey = "${agencyId}_$group"
-    val pattern = groupsProperties.getProperty(agencyGroupKey)
-      ?: throw EntityNotFoundException("No mappings found for $agencyGroupKey")
-
-    val locationPrefix = pattern
-      .replace(".", "")
-      .replace("+", "")
-
-    return LocationPrefixDto(locationPrefix)
-  }
 
   fun getCellLocationsForGroup(agencyId: String, groupName: String): List<Location> =
     prisonApiService.getAgencyLocationsForType(agencyId, "CELL")
