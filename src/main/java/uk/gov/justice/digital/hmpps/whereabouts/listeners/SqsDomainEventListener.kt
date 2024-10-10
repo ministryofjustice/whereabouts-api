e4e6c111794cfa5f27382874669711f0342af857
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-10-10 16:30:12 +0100

    BAVL-376 migration changes to cater for deleted bookings missing their corresponding delete event. (#735)

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/listeners/SqsDomainEventListener.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/listeners/SqsDomainEventListener.kt
@@ -23,31 +23,32 @@ class SqsDomainEventListener(
   }
 
   init {
-    log.info("BVLS enabled=$bvlsEnabled for domain events")
+    log.info("Legacy BVLS enabled=$bvlsEnabled for domain events")
   }
 
   @SqsListener("domainevent", factory = "hmppsQueueContainerFactoryProxy")
   @WithSpan(value = "map-devs-hmpps_whereabouts_api_domain_queue", kind = SpanKind.SERVER)
   fun handleDomainEvents(requestJson: String?) {
-    if (bvlsEnabled) {
-      try {
-        log.info("Raw domain event message: {}", requestJson)
-        val (message, messageAttributes) = gson.fromJson(requestJson, Message::class.java)
-        val eventType = messageAttributes.eventType.value
-        log.info("Processing message of type {}", eventType)
+    try {
+      log.info("Raw domain event message: {}", requestJson)
+      val (message, messageAttributes) = gson.fromJson(requestJson, Message::class.java)
+      val eventType = messageAttributes.eventType.value
+      log.info("Processing message of type {}", eventType)
 
-        when (eventType) {
-          "prison-offender-events.prisoner.released" -> {
-            val transferEventMessage = gson.fromJson(message, ReleasedOffenderEventMessage::class.java)
+      when (eventType) {
+        "prison-offender-events.prisoner.released" -> {
+          val transferEventMessage = gson.fromJson(message, ReleasedOffenderEventMessage::class.java)
+
+          if (bvlsEnabled) {
             videoLinkBookingService.deleteAppointmentWhenTransferredOrReleased(transferEventMessage)
+          } else {
+            log.info("Legacy BVLS event processing is disabled, ignoring release event - {}", transferEventMessage)
           }
         }
-      } catch (e: Exception) {
-        log.error("processDomainEvent() Unexpected error", e)
-        throw e
       }
-    } else {
-      log.info("Ignoring domain events for BVLS, BVLS feature is disabled.")
+    } catch (e: Exception) {
+      log.error("processDomainEvent() Unexpected error", e)
+      throw e
     }
   }
 }
