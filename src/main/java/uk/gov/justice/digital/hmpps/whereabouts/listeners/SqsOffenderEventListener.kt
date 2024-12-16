88806169ea084719bb73f80f0fba6eeaf2fde9a3
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-12-16 13:44:05 +0000

    BAVL-476 remove SNS subcription to domain events for BVLS code.

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/listeners/SqsOffenderEventListener.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/listeners/SqsOffenderEventListener.kt
@@ -7,26 +7,17 @@ import io.opentelemetry.instrumentation.annotations.WithSpan
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.whereabouts.services.AttendanceService
-import uk.gov.justice.digital.hmpps.whereabouts.services.court.VideoLinkBookingService
 
 @Service
 class SqsOffenderEventListener(
   @Qualifier("attendanceServiceAppScope")
   private val attendanceService: AttendanceService,
-  @Qualifier("videoLinkBookingServiceAppScope")
-  private val videoLinkBookingService: VideoLinkBookingService,
   private val gson: Gson,
-  @Value("\${feature.listen.for.court.events}") private val featureListenForCourtEvents: Boolean,
 ) {
   companion object {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
-  }
-
-  init {
-    log.info("featureListenForCourtEvents = $featureListenForCourtEvents")
   }
 
   @SqsListener("whereabouts", factory = "hmppsQueueContainerFactoryProxy")
@@ -47,16 +38,6 @@ class SqsOffenderEventListener(
             offenderIdDisplay,
             bookingIds,
           )
-        }
-
-        "APPOINTMENT_CHANGED" -> {
-          if (featureListenForCourtEvents) {
-            val appointmentChangedEventMessage = gson.fromJson(message, AppointmentChangedEventMessage::class.java)
-            videoLinkBookingService.processNomisUpdate(appointmentChangedEventMessage)
-            log.info("SQS event received. APPOINTMENT_CHANGED. processing appointmentChangedEventMessage $appointmentChangedEventMessage")
-          } else {
-            log.info("Ignoring offender event appointment changed for BVLS, as featureListenForCourtEvent is disabled")
-          }
         }
       }
     } catch (e: Exception) {
