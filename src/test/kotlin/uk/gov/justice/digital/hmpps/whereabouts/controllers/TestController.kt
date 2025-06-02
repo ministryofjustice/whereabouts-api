bb15ca3b4618d003b4eb5e3a5fc2675d9709a943
munslowl <lance.munslow@digital.justice.gov.uk>
2025-06-02 11:57:00 +0100

    Fix tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/controllers/TestController.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/whereabouts/controllers/TestController.kt
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
+import uk.gov.justice.digital.hmpps.whereabouts.config.ResourceServerConfiguration
+import uk.gov.justice.digital.hmpps.whereabouts.utils.JwtAuthHelper
 import uk.gov.justice.digital.hmpps.whereabouts.utils.UserMdcFilter
 
 @ActiveProfiles("test")
-@Import(UserMdcFilter::class, StubUserSecurityUtilsConfig::class)
+@Import(UserMdcFilter::class, StubUserSecurityUtilsConfig::class, ResourceServerConfiguration::class, JwtAuthHelper::class)
 open class TestController {
   @Autowired
-  internal lateinit var mockMvc: MockMvc
+  lateinit var mockMvc: MockMvc
 
   @Autowired
-  internal lateinit var objectMapper: ObjectMapper
+  lateinit var objectMapper: ObjectMapper
 }
