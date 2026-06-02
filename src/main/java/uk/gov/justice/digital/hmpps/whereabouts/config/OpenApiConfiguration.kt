55bf7d6ebf9d3531b489d090c5d0684526017ffb
munslowl <lance.munslow@digital.justice.gov.uk>
2026-06-02 14:09:54 +0100

    Fix Open API spec

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/OpenApiConfiguration.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/OpenApiConfiguration.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.whereabouts.config
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
@@ -16,17 +17,25 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
   fun customOpenAPI(): OpenAPI = OpenAPI()
     .servers(
       listOf(
-        Server().url("https://template-kotlin-dev.hmpps.service.justice.gov.uk").description("Development"),
-        Server().url("https://template-kotlin-preprod.hmpps.service.justice.gov.uk").description("Pre-Production"),
-        Server().url("https://template-kotlin.hmpps.service.justice.gov.uk").description("Production"),
-        Server().url("http://localhost:8080").description("Local"),
+        Server().url("https://whereabouts-api-dev.service.justice.gov.uk").description("Development"),
+        Server().url("https://whereabouts-api-preprod.service.justice.gov.uk").description("Pre-Production"),
+        Server().url("https://whereabouts-api.service.justice.gov.uk").description("Production"),
+        Server().url("http://localhost:8082").description("Local"),
       ),
     )
     .tags(
       listOf(),
     )
     .info(
-      Info().title("HMPPS Template Kotlin").version(version)
-        .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
+      Info()
+        .title("HMPPS Whereabouts API")
+        .version(version)
+        .description("API for prisoner whereabouts")
+        .contact(
+          Contact()
+            .name("HMPPS Digital Studio")
+            .email("feedback@digital.justice.gov.uk"),
+        ),
     )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
 }
