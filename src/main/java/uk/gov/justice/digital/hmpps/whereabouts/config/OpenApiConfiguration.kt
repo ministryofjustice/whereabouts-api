984d052844cedc4d5016bfd17329cfe34a3a82c0
munslowl <lance.munslow@digital.justice.gov.uk>
2026-06-02 14:19:35 +0100

    Add auth details to Open API spec

--- src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/OpenApiConfiguration.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/whereabouts/config/OpenApiConfiguration.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.whereabouts.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
@@ -15,6 +17,17 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
 
   @Bean
   fun customOpenAPI(): OpenAPI = OpenAPI()
+    .components(
+      Components().addSecuritySchemes(
+        "bearer-jwt",
+        SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+          .`in`(SecurityScheme.In.HEADER)
+          .name("Authorization"),
+      ),
+    )
     .servers(
       listOf(
         Server().url("https://whereabouts-api-dev.service.justice.gov.uk").description("Development"),
